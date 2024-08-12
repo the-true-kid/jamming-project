5eafd9f756196adb920411bd9eac2158fc3a1d48
the-true-kid <aaronweiss00@gmail.com>
2024-08-12 13:28:13 -0400

    added URI functionality

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,11 @@ import './App.css';
 
 function App() {
   const [searchResults, setSearchResults] = useState([
-    { id: 1, name: 'Song 1', artist: 'Artist 1', album: 'Album 1' },
-    { id: 2, name: 'Song 2', artist: 'Artist 2', album: 'Album 2' },
-    { id: 3, name: 'Song 3', artist: 'Artist 3', album: 'Album 3' },
+    { id: 1, name: 'Song 1', artist: 'Artist 1', album: 'Album 1', uri: 'spotify:track:1czLCvl9wlBix1BwRhVXjH' },
+    { id: 2, name: 'Song 2', artist: 'Artist 2', album: 'Album 2', uri: 'spotify:track:2TpxZ7JUBn3uw46aR7qd6V' },
+    { id: 3, name: 'Song 3', artist: 'Artist 3', album: 'Album 3', uri: 'spotify:track:3n3Ppam7vgaVa1iaRUc9Lp' },
   ]);
+  
 
   const [playlistTracks, setPlaylistTracks] = useState([]);
   const [playlistName, setPlaylistName] = useState('New Playlist');
@@ -31,9 +32,16 @@ function App() {
   const savePlaylist = () => {
     const trackURIs = playlistTracks.map(track => track.uri);
     console.log(`Saving playlist: ${playlistName} with tracks: ${trackURIs}`);
-    // Implement actual Spotify saving functionality here
+    
+    // You would typically call a Spotify API method here to save the playlist
+    // For example:
+    // Spotify.savePlaylist(playlistName, trackURIs);
+  
+    // Resetting the playlist after saving
+    setPlaylistName('New Playlist');
+    setPlaylistTracks([]);
   };
-
+  
   const search = (term) => {
     console.log(`Searching for ${term}`);
     // Implement search functionality with Spotify API here
